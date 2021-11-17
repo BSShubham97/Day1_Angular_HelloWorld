d508e966fadcaff74488997b4ef2bd342b970c2c
Shubham Bandagale <sbandagale18@gmail.com>
2021-11-17 10:54:49 +0530

    ADDED: Hellow World Data Binding

--- HelloWorldAngular/src/app/app.component.ts
+++ HelloWorldAngular/src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HelloWorldAngular';
+  title = "HelloWorld";
+  imgUrl= "";
+  url: string | URL | undefined;
+  userName: string = "";
+  nameError: string= "";
+
+ ngOnInit(): void{
+  this.title = 'HELLO FROM BRIDGELABZ'
+  this.imgUrl = "assets/BridgeLabz-2.jpg"
+  this.url="https://www.bridgelabz.com/";  
+}
+ onClick($event: any){
+  console.log("SAVE BUTTON IS CLICKED",$event);
+  window.open(this.url, "_blank");
+}
+ onInput($event:any){
+  console.log("Change Event Occured!", $event.data);
+  const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+  console.log(nameRegex.test(this.userName));
+  
+  if(nameRegex.test(this.userName)){
+    this.nameError = "";
+    return; 
+  }
+  this.nameError = "NAME IS INCORRECT";
+}
 }
